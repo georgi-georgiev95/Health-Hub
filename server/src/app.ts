f5f7dafe0a213e49dfd33941f13ca50ffe59f602
Georgi957 <gesh007@abv.bg>
2024-05-03 18:11:59 +0300

    Connect DB

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const dotenv = require("dotenv");
 dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Connected to MongoDB");
+})
 
 const port: string = process.env.PORT || "4000";
 
