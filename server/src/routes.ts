a4827673af340f1fbfef3c4a7928963d9641282a
Georgi957 <gesh007@abv.bg>
2024-05-03 18:35:05 +0300

    Add workout controller

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,9 +3,11 @@ import { Request, Response } from "express";
 const router = require("express").Router();
 const userController = require("./controllers/userController");
 const recipeController = require("./controllers/recipeController");
+const workoutController = require("./controllers/workoutController");
 
 router.use("/users", userController);
 router.use("/recipes", recipeController);
+router.use("/workouts", workoutController);
 router.get("/", (req: Request, res: Response) => {
     res.json({ message: "Hello World!" });
 })
